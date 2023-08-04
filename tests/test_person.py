0a2b07013cfc8ec7a15816fcea5fdf27df8911a5
lplancarte <lplancarte@outlook.com>
2023-08-04 11:27:52 -0600

    added fixture decorator and test_surname function

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,8 +1,22 @@
+import pytest
 from pytest_github_actions.person import Person
 
 
-def test_init():
-    person = Person("Luis Plancarte", 26, jobs=["Software Engineer"])
+@pytest.fixture()
+# fixture decorator allows to instantiate an object only once
+def person():
+    return Person("Luis Plancarte", 26, jobs=["Software Engineer"])
+
+
+def test_init(person: Person):
     assert person.name == "Luis Plancarte"
     assert person.age == 26
     assert person.jobs == ["Software Engineer"]
+
+
+def test_forename(person: Person):
+    assert person.forename == "Luis"
+
+
+def test_surname(person: Person):
+    assert person.surname == "Plancarte"
